9d92fd1980331017494fa2c60ccfda56ed42e3e0
Alexandros-Eu <alexeouenta@gmail.com>
2025-04-21 15:59:41 +0300

    Add Summary component

---
+++ src/components/Summary.jsx
@@ -0,0 +1,11 @@
+import logo from '../assets/quiz-complete.png';
+
+export default Summary()
+{
+    return (
+        <div id="summary">
+            <img src={logo} alt="Completion Logo" />
+            <h2>Quiz Completed!</h2>
+        </div>
+    )
+}
