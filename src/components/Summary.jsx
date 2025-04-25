c935f2053f74d90d66dadff4640123be581d8158
Alexandros-Eu <alexeouenta@gmail.com>
2025-04-26 02:54:22 +0300

    End Summary component and calculations logic of results

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,30 +1,61 @@
 import logo from '../assets/quiz-complete.png';
 import { questions, answers } from '../QuizData.js';
 
+let correctPerc =  undefined;
+let incorrectPerc =  undefined;
+let skippedPerc =  undefined;
+
 function getAnswers()
 {
     return JSON.parse(localStorage.getItem("userAnswers"));
 }
 
+function calculatePerc()
+{
+    let correctAnswers = 0;
+    let incorrectAnswers = 0;
+    let skipped = 0;
+
+    questions.map((question, i) => {
+        if(userAnswers[i] === answers[i])
+        {
+            correctAnswers++;
+        }
+        else if(userAnswers[i] === null)
+        {
+            skipped++;
+        }
+        else
+        {
+            incorrectAnswers++;
+        }
+    })
+
+    correctPerc = Math.floor(correctAnswers / questions.length * 100);
+    incorrectPerc = Math.floor(incorrectAnswers / questions.length * 100);
+    skippedPerc = Math.floor(skipped / questions.length * 100);
+}
+
+const userAnswers = getAnswers();
+calculatePerc();
+
 export default function Summary()
 {
-    const userAnswers = getAnswers();
-
     return (
         <div id="summary">
             <img src={logo} alt="Completion Logo" />
             <h2>Quiz Completed!</h2>
             <div id="summary-stats">
                 <p>
-                    <span className="number">0%</span>
+                    <span className="number">{skippedPerc}%</span>
                     <span className="text">skipped</span>
                 </p>
                 <p>
-                    <span className="number">43%</span>
+                    <span className="number">{correctPerc}%</span>
                     <span className="text">answered correctly</span>
                 </p>
                 <p>
-                    <span className="number">57%</span>
+                    <span className="number">{incorrectPerc}%</span>
                     <span className="text">answered incorrectly</span>
                 </p>
             </div>
