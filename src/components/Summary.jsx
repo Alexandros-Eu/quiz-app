b620fa1a72b5b593bcbed796a682367ed847cb0a
Alexandros-Eu <alexeouenta@gmail.com>
2025-04-26 02:04:20 +0300

    Start Summary component logic and functionality

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,7 +1,15 @@
 import logo from '../assets/quiz-complete.png';
+import { questions, answers } from '../QuizData.js';
+
+function getAnswers()
+{
+    return JSON.parse(localStorage.getItem("userAnswers"));
+}
 
 export default function Summary()
 {
+    const userAnswers = getAnswers();
+
     return (
         <div id="summary">
             <img src={logo} alt="Completion Logo" />
@@ -20,6 +28,18 @@ export default function Summary()
                     <span className="text">answered incorrectly</span>
                 </p>
             </div>
+            {questions.map((question, i) => {
+
+                let isCorrect = answers[i] === userAnswers[i];
+
+                return (
+                    <ol>
+                        <h3>{i + 1}</h3>
+                        <li className="question">{question}</li>
+                        <li className={`user-answer ${isCorrect ? "correct" : "wrong"}`}>{userAnswers[i]}</li>
+                    </ol>
+                )
+            })}
         </div>
     )
 }
