cd50cdd38cb336d6ee57d6509171c1249f57986a
Alexandros-Eu <alexeouenta@gmail.com>
2025-04-26 21:22:10 +0300

    Update Quiz data with new questions

--- src/QuizData.js
+++ src/QuizData.js
@@ -1,17 +1,23 @@
 export const questions = [
-    "What is the most populous Country on Earth?",
-    "Which country has the biggest Economy on the planet?",
-    "What is the dark form of Democracy?"
+    "Which country currently boasts the world's largest population?",
+    "When measuring economic output, which nation's economy stands as the largest globally?",
+    "What term describes the less desirable, or 'darker,' aspects of democratic rule?",
+    "In terms of geographical size, which country encompasses the largest landmass?",
+    "Among sovereign states, which nation holds the largest verified reserves of crude oil?"
 ]
 
 export const answers = [
     "India", 
     "USA", 
-    "Ochlocracy"
+    "Ochlocracy",
+    "Russia",
+    "Venezuela"
 ]
 
 export const potentialAnswers = [
     ["India", "China", "USA", "Germany"],
     ["USA", "China", "Japan", "EU"],
-    ["Oligarchy", "Tyranny", "Ochlocracy", "Aristocracy"]
+    ["Oligarchy", "Tyranny", "Ochlocracy", "Aristocracy"],
+    ["Australia", "Nigeria", "Russia", "USA"],
+    ["Saudi Arabia", "Iran", "UAE", "Venezuela"]
 ]
