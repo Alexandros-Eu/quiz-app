4712c95be6c1adcb2860d7cf2038359b58bb6c39
Alexandros-Eu <alexeouenta@gmail.com>
2025-05-03 16:09:45 +0300

    Add QuizTimer progress bar

---
+++ src/components/QuizTimer.jsx
@@ -0,0 +1,28 @@
+import { useState, useEffect } from 'react';
+
+export default function QuizTimer({timeout, onTimeout})
+{
+    const [timeRemaining, setTimeRemaining] = useState(timeout);
+
+    useEffect(() => {
+        console.log("SETTING TIMEOUT");
+        const timer = setTimeout(() => {
+            onTimeout();
+        }, timeout)
+
+        return () => clearTimeout(timer);
+
+    }, [timeout, onTimeout])
+
+    useEffect(() => {
+        console.log("SETTING INTERVAL");
+        const interval = setInterval(() => {
+            setTimeRemaining(prevTimeRemaining => prevTimeRemaining - 100);
+        }, 100)
+
+        return () => clearInterval(interval);
+    }, [])
+
+
+    return <progress id="question-timer" value={timeRemaining} max={timeout}/>
+}
